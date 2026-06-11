ac4882ac14e4825cb58ce2495c8352f53031734a
Walinson Pereira <walinsonoliveirapereira@gmail.com>
2026-06-11 18:40:06 -0300

    🔨 refactor(metro-config): correcao leitura imagens

--- ArquivoBilu/metro.config.js
+++ ArquivoBilu/metro.config.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-// 1. Garante que o Metro saiba ler os arquivos de imagem
 config.resolver.assetExts.push("jpg", "jpeg", "png", "asset");
 
-// 2. FORÇA INSTÂNCIA ÚNICA: Resolve o Three.js estritamente para a raiz do projeto
+config.watchFolders = [
+  path.resolve(__dirname, "assets"),
+];
+
 config.resolver.resolverMainFields = ['sbmodern', 'browser', 'main'];
 
 config.resolver.extraNodeModules = {
@@ -14,7 +16,6 @@ config.resolver.extraNodeModules = {
   'three': path.resolve(__dirname, 'node_modules/three'),
 };
 
-// Evita que pacotes dentro de node_modules usem suas próprias versões internas do Three
 config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, 'node_modules'),
 ];
