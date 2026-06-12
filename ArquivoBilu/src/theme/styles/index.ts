69e4be2b9d8882dc4eac6ac240eb370b432066d0
Walinson Pereira <walinsonoliveirapereira@gmail.com>
2026-06-12 17:07:57 -0300

    ✨ feat: adicao exportacao global estilos

---
+++ ArquivoBilu/src/theme/styles/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @file index.ts
+ * @description Ponto de entrada para todos os estilos globais compartilhados.
+ *
+ * Permite imports limpos em qualquer tela ou componente:
+ *
+ * @example
+ *   import { feedbackStyles, cardStyles, layoutStyles, formStyles } from "@/theme/styles";
+ */
+
+export { feedbackStyles } from "./feedback";
+export { cardStyles } from "./cards";
+export { layoutStyles } from "./layout";
+export { formStyles } from "./forms";
